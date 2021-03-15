773e02d9c7611d7697847dbbf8165f8fa07f28ac
Ville <ville.2000.vhk@gmail.com>
2021-03-15 22:32:23 +0200

    fix

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.put('/api/persons/:id', (req, res) =>{
     res.json(newPerson)
     
 })
-const PORT =  process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log("Server started at port " + PORT)
 })
